4d4cbf9af5f524d44182e5d93e5767f13f70512a
d40cht <d40cht@94eed753-664e-0410-94e1-b9a694de5147>
2008-06-13 16:20:27 +0000

    ...

--- testing/userextractor.cpp
+++ testing/userextractor.cpp
@@ -7,6 +7,7 @@
 #include <boost/shared_ptr.hpp>
 #include <boost/bind.hpp>
 #include <boost/foreach.hpp>
+#include <boost/date_time/gregorian/gregorian_types.hpp>
 
 
 void readOSMXML( XercesInitWrapper &x, const std::string &fileName, OSMFragment &frag )
@@ -44,8 +45,44 @@ int main( int argc, char **argv )
          
         readOSMXML( x, fileName, fragment );
 
+        boost::posix_time::ptime now(
+            boost::gregorian::date( 2008, 06, 13 ),
+            boost::posix_time::time_duration( 17, 0, 0 ) );
+
+        typedef boost::tuples::cons<
+            std::string,              boost::tuples::cons<
+            id_t,                     boost::tuples::cons<
+            int,                      boost::tuples::cons<
+            std::string,              boost::tuples::cons<
+            boost::posix_time::ptime, boost::tuples::cons<
+            int,                      boost::tuples::cons<
+            std::string,              boost::tuples::cons<
+            double,                   boost::tuples::cons<
+            double,                   boost::tuples::cons<
+            int,                      boost::tuples::cons<
+            int,                      boost::tuples::cons<
+            std::string,              boost::tuples::cons<
+            std::string,              boost::tuples::null_type> > > > > > > > > > > > > userRow_t;
+
+        std::vector<userRow_t> userRows;
         BOOST_FOREACH( const OSMFragment::userMap_t::value_type &v, fragment.getUsers() )
         {
+            userRow_t newUser = userRow_t( boost::make_tuple(
+                "blah@blah.com",
+                v.first,
+                1,
+                "DKJHSDKJS",
+                now,
+                v.second,
+                1,
+                "A bloke or a girl",
+                0.0,
+                0.0, boost::make_tuple(
+                0,
+                0,
+                "shdkasj",
+                "" ) );
+
             std::cout << "User id: " << v.first << ", with name: " << v.second << std::endl;
         }
     }
