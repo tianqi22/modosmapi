34fa5993593f42bbcc0fd2fa9e6b9e04268d72a8
d40cht <d40cht@94eed753-664e-0410-94e1-b9a694de5147>
2008-07-17 06:38:06 +0000

    Simple const string map class added for more efficient storage and retrieval of tags

--- trunk/mod_osm/utils.cpp
+++ trunk/mod_osm/utils.cpp
@@ -1,5 +1,6 @@
 #include <cmath>
 
+#include <utils.hpp>
 
 const double PI = acos( -1.0 );
 
@@ -16,3 +17,51 @@ double distBetween( double lat1, double lon1, double lat2, double lon2 )
 
     return 6378.0 * acos(cos(a1)*cos(b1)*cos(a2)*cos(b2) + cos(a1)*sin(b1)*cos(a2)*sin(b2) + sin(a1)*sin(a2));
 }
+
+std::vector<std::string>    ConstTagString::m_theStrings;
+size_t                      ConstTagString::m_lastIndex = 0;
+ConstTagString::stringMap_t ConstTagString::m_stringIndexMap;
+
+ConstTagString::ConstTagString( const std::string &str )
+{
+    assignString( str );
+}
+
+ConstTagString::ConstTagString( const ConstTagString &rhs )
+{
+    m_stringIndex = rhs.m_stringIndex;
+}
+
+ConstTagString &ConstTagString::operator=( const ConstTagString &rhs )
+{
+    m_stringIndex = rhs.m_stringIndex;
+    return *this;
+}
+
+void ConstTagString::assignString( const std::string &str )
+{
+    stringMap_t::iterator findIt = m_stringIndexMap.find( str );
+    
+    if ( findIt == m_stringIndexMap.end() )
+    {
+        m_theStrings.push_back( str );
+        m_stringIndex = m_lastIndex++;
+        m_stringIndexMap.insert( std::make_pair( str, m_stringIndex ) );            
+    }
+    else
+    {
+        m_stringIndex = findIt->second;
+    }
+}
+
+bool ConstTagString::operator==( const ConstTagString &rhs )
+{
+    return m_stringIndex == rhs.m_stringIndex;
+}
+
+bool ConstTagString::operator<( const ConstTagString &rhs )
+{
+    // NOTE: operator< does not give lexicographic ordering
+    return m_stringIndex < rhs.m_stringIndex;
+}
+
--- trunk/mod_osm/utils.hpp
+++ trunk/mod_osm/utils.hpp
@@ -1,3 +1,33 @@
+#include <map>
+#include <vector>
+#include <string>
 
-extern double PI;
+extern const double PI;
 double distBetween( double, double, double, double );
+
+
+class ConstTagString
+{
+private:
+    typedef std::map<std::string, size_t> stringMap_t;
+
+    static std::vector<std::string> m_theStrings;
+    static size_t m_lastIndex;
+    static stringMap_t m_stringIndexMap;
+    
+    size_t m_stringIndex;
+
+public:
+    ConstTagString( const std::string &str );
+    ConstTagString( const ConstTagString &rhs );
+    ConstTagString &operator=( const ConstTagString &rhs );
+    bool operator==( const ConstTagString &rhs );
+    bool operator<( const ConstTagString &rhs );
+
+private:
+    void assignString( const std::string &str );
+};
+
+
+
+
